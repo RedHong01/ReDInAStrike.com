8e26c6c5743ba4fc783da6656f1aa289df5b45b2
刹那繁红 <92612697+RedHong01@users.noreply.github.com>
2026-08-27 23:56:12 -0700

    Expose motion defaults in GitHub Pages build

---
+++ docs/src/motion-default.js
@@ -0,0 +1,1 @@
+export { MOTION_GROUPS, MOTION_PARAM_META, PUBLISHED_MOTION_CONFIG, sanitizeMotionConfig, cloneMotionConfig, encodeMotionConfig, decodeMotionConfig } from "../../src/motion-default.js"
